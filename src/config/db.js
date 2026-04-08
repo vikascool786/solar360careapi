3cc52919b1a13b3ba3b98be204e929ba1173dca3
vikasmamerica <vikasw@marketamerica.com>
2026-04-08 18:04:58 +0530

    all

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool.promise();

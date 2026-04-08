ec1eeb6fe1b5bba94f9bb28e2ce5e3e02816178a
vikasmamerica <vikasw@marketamerica.com>
2026-04-08 10:14:13 +0530

    all

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use("/api/templates", require("./routes/template.routes"));
 // cron job running
 require('./services/reminderService'); // start the reminder service
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5004;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

7d1d287572e632623bdacb768f478d374b6c9afc
vikasmamerica <vikasw@marketamerica.com>
2026-04-08 18:31:29 +0530

    all

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "admin",
+  password: "Poonam#@#1988",
+  database: "solar_crm",
 
   waitForConnections: true,
   connectionLimit: 10,

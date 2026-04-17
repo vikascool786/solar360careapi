79f420871e9d9be42413c3b6072d32e829ef7833
vikasmamerica <vikasw@marketamerica.com>
2026-04-17 10:19:53 +0530

    env and db.js updated

--- src/config/db-local.js
+++ src/config/db-local.js
@@ -1,3 +1,18 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "admin",
+//   password: "Poonam#@#1988",
+//   database: "solar360crm",
+
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = pool.promise();
+
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,25 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "admin",
+//   password: "Poonam#@#1988",
+//   database: "solar360crm",
+
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = pool.promise();
+
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "admin",
-  password: "Poonam#@#1988",
-  database: "solar360crm",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
   waitForConnections: true,
   connectionLimit: 10,
